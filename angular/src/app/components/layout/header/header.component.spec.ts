2c8ffeb32cce28e267cadf21c66fe79c6269412c
Ronan Brett <Ronan.Brett@theaa.ie>
2017-05-05 16:02:20 +0100

    chore(Imgs) Update Images

--- angular/src/app/components/layout/header/header.component.spec.ts
+++ angular/src/app/components/layout/header/header.component.spec.ts
@@ -1,10 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +18,23 @@ describe('HeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('DOM', () => {
+    it('should have a title', () => {
+      expect(debugElement.query(By.css('.header__title')).nativeElement.innerText).toContain('S.');
+    });
+
+    it('should have a set of links', () => {
+      const headerItems = debugElement.queryAll(By.css('.header__links a'));
+      expect(headerItems.length).toBe(2);
+    });
+  });
+
 });
---
+++ angular/src/app/pages/pages.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePageModule } from './home-page/home-page.module';
+
+@NgModule({
+	imports: [
+		CommonModule,
+		HomePageModule
+	],
+	declarations: []
+})
+export class PagesModule { }
