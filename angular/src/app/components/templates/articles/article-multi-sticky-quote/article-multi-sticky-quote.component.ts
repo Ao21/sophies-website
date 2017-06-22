ef37497f1bdca2ffb48a0d9789adf895b1065480
Ronan Brett <Ronan.Brett@theaa.ie>
2017-06-22 11:38:39 +0100

    feat(Article) Use Grids in multi sticky quote template

--- angular/src/app/components/templates/articles/article-multi-sticky-quote/article-multi-sticky-quote.component.ts
+++ angular/src/app/components/templates/articles/article-multi-sticky-quote/article-multi-sticky-quote.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'article-multi-sticky-quote',
-  templateUrl: './article-multi-sticky-quote.component.html',
-  styleUrls: ['./article-multi-sticky-quote.component.scss']
+  templateUrl: './article-multi-sticky-quote.component.grid.html',
+  styleUrls: ['./article-multi-sticky-quote.component.grid.scss']
 })
 export class ArticleMultiStickyQuoteComponent implements OnInit {
 
