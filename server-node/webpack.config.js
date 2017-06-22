66ccb0c30891533600b02776b4748c64ca568fb6
Ronan Brett <Ronan.Brett@theaa.ie>
2017-06-22 17:06:19 +0100

    feat(Sever) Create Node Server

---
+++ server-node/webpack.config.js
@@ -0,0 +1,54 @@
+var webpack = require("webpack");
+var path = require("path");
+var fs = require("fs");
+
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+var nodeModules = {};
+fs
+  .readdirSync("node_modules")
+  .filter(function(x) {
+    return [".bin"].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = "commonjs " + mod;
+  });
+
+module.exports = {
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  externals: nodeModules,
+
+  output: {
+    filename: "dist.js",
+    path: path.resolve(__dirname, "dist"),
+    devtoolModuleFilenameTemplate: "[absolute-resource-path]",
+    devtoolFallbackModuleFilenameTemplate: "[absolute-resource-path]?[hash]"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        enforce: "pre",
+        loader: "tslint-loader",
+        exclude: /node_modules/,
+        options: {
+          typeCheck: true
+        }
+      },
+      {
+        test: /\.ts?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: [".ts", ".js", ".json"],
+    modules: [path.join(__dirname, "src"), "node_modules"]
+  },
+
+  plugins: [new CleanWebpackPlugin(["dist"])]
+};
