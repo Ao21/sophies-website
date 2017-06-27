d4fcfe98db206a4b583f5122f906d8104d9a7f63
Ronan Brett <Ronan.Brett@theaa.ie>
2017-06-27 22:57:19 +0100

    feat(mongoose) Add Mongoose config

--- server-node/src/app/app.controller.ts
+++ server-node/src/app/app.controller.ts
@@ -1,11 +1,13 @@
 import * as express from "express";
+import * as mongoose from "mongoose";
 import { env } from "./../environments/environment";
 
-import { BaseAPI } from "./api/api.controller";
+import { BaseAPI } from "./api/api.service";
 
 export class Server {
     public app: express.Application;
-    public api: BaseAPI;
+	public api: BaseAPI;
+	public mongoose: mongoose.Mongoose = mongoose;
 
 	public static boostrap() {
 		return new Server();
@@ -19,6 +21,7 @@ export class Server {
         this.app.use(this.api.getRoutes());
 	}
 	config() {
+		this.mongoose.connect("mongodb://localhost/sophie");
 		this.app.listen(env.settings.node.port, function() {
 			console.log("Example app listening on port 3000!");
 		});
