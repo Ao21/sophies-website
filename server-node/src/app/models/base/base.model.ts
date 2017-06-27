4d7f1322972b51cd6fde2967e123a51d23ae2313
Ronan Brett <Ronan.Brett@theaa.ie>
2017-06-27 20:31:09 +0100

    fix(Housecleaning) Remove unused files

--- server-node/src/app/models/base/base.model.ts
+++
@@ -1,9 +0,0 @@
-import query from "./base.gql";
-
-export class BaseModel {
-    public root = query;
-
-    constructor() {
-        console.log(query);
-    }
-}
--- server-node/src/app/models/models.controller.ts
+++
@@ -1,8 +0,0 @@
-import { BaseModel } from "./base/base.model";
-
-export class Models {
-	public baseModel: BaseModel;
-	constructor() {
-		this.baseModel = new BaseModel();
-	}
-}
