07c02a015bbe893f024dae8cc562df45f5cfa7cd
Ronan Brett <Ronan.Brett@theaa.ie>
2017-06-30 16:06:31 +0100

    feat(CMS) Create Apollo Client

---
+++ angular/src/app/core/apollo/apollo.client.ts
@@ -0,0 +1,82 @@
+import { NgModule } from '@angular/core';
+
+import {
+	ApolloClient,
+	createNetworkInterface,
+	IntrospectionFragmentMatcher
+} from 'apollo-client';
+import { ApolloModule } from 'apollo-angular';
+
+const myFragmentMatcher = new IntrospectionFragmentMatcher({
+	introspectionQueryResultData: {
+		__schema: {
+			types: [
+				{
+					kind: 'UNION',
+					name: 'Field',
+					possibleTypes: [
+						{
+							name: 'TextField'
+						},
+						{
+							name: 'DateField'
+						},
+						{
+							name: 'FieldGroup'
+						}
+					]
+				},
+				{
+					kind: 'INTERFACE',
+					name: 'FieldInterface',
+					possibleTypes: [
+						{
+							name: 'TextField'
+						},
+						{
+							name: 'DateField'
+						},
+						{
+							name: 'FieldGroup'
+						}
+					]
+				},
+				{
+					kind: 'INTERFACE',
+					name: 'BlockInterface',
+					possibleTypes: [
+						{
+							name: 'Block'
+						}
+					]
+				},
+				{
+					kind: 'INTERFACE',
+					name: 'ArticleInterface',
+					possibleTypes: [
+						{
+							name: 'Article'
+						}
+					]
+				}
+			]
+		}
+	}
+});
+
+export const client = new ApolloClient({
+	fragmentMatcher: myFragmentMatcher,
+	networkInterface: createNetworkInterface({
+		uri: '/api/graphql'
+	})
+});
+
+export function provideClient(): ApolloClient {
+	return client;
+}
+
+@NgModule({
+	imports: [ApolloModule.forRoot(provideClient)],
+	exports: [ApolloModule]
+})
+export class Apollo {}
