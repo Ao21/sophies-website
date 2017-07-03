74e7832e967cfd5bd27d2edab4f55b1a1f788ac8
Ronan Brett <Ronan.Brett@theaa.ie>
2017-07-03 13:51:41 +0100

    chore(Form) Update dynamic form to use new fields

--- angular/src/app/components/form/dynamic-form/dynamic-form.component.ts
+++ angular/src/app/components/form/dynamic-form/dynamic-form.component.ts
@@ -1,15 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit} from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FieldControlService } from './../../../services/field-control.service';
+
+import { BaseField } from './../models/';
+
+import { Subscription } from 'rxjs/Rx';
 
 @Component({
-  selector: 'dynamic-form',
-  templateUrl: './dynamic-form.component.html',
-  styleUrls: ['./dynamic-form.component.scss']
+	selector: 'dynamic-form',
+	templateUrl: './dynamic-form.component.html',
+	providers: [FieldControlService],
+	styleUrls: ['./dynamic-form.component.scss']
 })
-export class DynamicFormComponent implements OnInit {
+export class DynamicFormComponent implements OnInit, AfterViewInit {
+	@Input() form: FormGroup;
+	subscriptions: Subscription[] = [];
+	questions: BaseField<any>[] = [
+		{
+			id: 'field',
+			type: 'text',
+			key: 'key',
+			config: {
+				label: 'Title',
+				placeholder: 'Title of the article',
+			}
+		},
+		{
+			id: 'toggleField',
+			type: 'toggle',
+			key: 'toggleFieldKey',
+			config: {
+				label: 'Featured',
+				description: 'Should this article featured on the main page?'
+			}
+		},
+		{
+			id: 'textAreaField',
+			type: 'textarea',
+			key: 'textAreaField',
+			config: {
+				label: 'Featured',
+				description: 'Should this article featured on the main page?'
+			}
+		},
+		{
+			id: 'checkboxField',
+			type: 'checkbox',
+			key: 'checkBoxField',
+			config: {
+				label: 'Featured',
+				description: 'Should this article featured on the main page?'
+			}
+		},
+		{
+			id: 'dateField',
+			type: 'date',
+			key: 'dateField',
+			config: {
+				label: 'Featured',
+				description: 'Should this article featured on the main page?'
+			}
+		}
+	];
 
-  constructor() { }
+	constructor(private fieldControlService: FieldControlService) {}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.form = this.fieldControlService.toFormGroup(this.fieldControlService.getFields(this.questions));
+	}
 
+	ngAfterViewInit() {
+		this.initSubs();
+	}
+
+	initSubs() {
+		const fieldValueChangeSub = this.form.valueChanges.subscribe((next) => {
+			console.log(next);
+		});
+	}
+	getFormControl(key: string) {
+		return this.form.get(key);
+	}
 }
--- angular/src/app/components/form/dynamic-form/dynamic-form.module.ts
+++ angular/src/app/components/form/dynamic-form/dynamic-form.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { DynamicFormComponent } from './dynamic-form.component';
 
+import { InputFieldModule } from './../fields/input-field/input-field.module';
+import { ToggleFieldModule } from './../fields/toggle-field/toggle-field.module';
+import { QuillModule } from './../../externals/quill/quill.module';
+import { CheckboxFieldModule } from './../fields/checkbox-field/checkbox-field.module';
+import { DateFieldModule } from './../fields/date-field/date-field.module';
+
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [DynamicFormComponent],
-  exports: [DynamicFormComponent]
+	imports: [
+		CommonModule,
+		ReactiveFormsModule,
+		InputFieldModule,
+		ToggleFieldModule,
+		QuillModule,
+		CheckboxFieldModule,
+		DateFieldModule
+	],
+	declarations: [DynamicFormComponent],
+	exports: [DynamicFormComponent]
 })
-export class DynamicFormModule { }
+export class DynamicFormModule {}
