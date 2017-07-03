08191b633f787a249568af96143d49003fd1606e
Ronan Brett <Ronan.Brett@theaa.ie>
2017-07-03 13:52:02 +0100

    feat(CMS-entry) Update cms entry with new styles

--- angular/src/app/components/features/cms-entry/cms-entry.module.ts
+++ angular/src/app/components/features/cms-entry/cms-entry.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CmsEntryComponent } from './cms-entry.component';
 
+import { RouterModule } from '@angular/router';
+
+import { FormModule } from './../../form/form.module';
+
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [CmsEntryComponent]
+	imports: [CommonModule, FormModule, RouterModule],
+	declarations: [CmsEntryComponent]
 })
-export class CmsEntryModule { }
+export class CmsEntryModule {}
