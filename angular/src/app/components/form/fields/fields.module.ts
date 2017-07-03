4ef549017e354a9f5a3c07a44926c797801c6bdc
Ronan Brett <Ronan.Brett@theaa.ie>
2017-07-03 13:51:10 +0100

    chore(Fields) Update field container module

--- angular/src/app/components/form/fields/fields.module.ts
+++ angular/src/app/components/form/fields/fields.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputFieldModule } from './input-field/input-field.module';
+import { CheckboxFieldModule } from './checkbox-field/checkbox-field.module';
+import { ToggleFieldModule } from './toggle-field/toggle-field.module';
+import { DateFieldModule } from './date-field/date-field.module';
 
 @NgModule({
-	imports: [CommonModule, InputFieldModule],
-  declarations: [],
-  exports: [InputFieldModule]
+	imports: [
+		CommonModule,
+		InputFieldModule,
+		CheckboxFieldModule,
+		ToggleFieldModule,
+		DateFieldModule
+	],
+	declarations: [],
+	exports: [
+		InputFieldModule,
+		CheckboxFieldModule,
+		ToggleFieldModule,
+		DateFieldModule
+	]
 })
 export class FieldsModule {}
