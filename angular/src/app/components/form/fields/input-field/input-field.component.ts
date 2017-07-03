0fc16d306a150a9a3688bc237596f6d33b78e8fa
Ronan Brett <Ronan.Brett@theaa.ie>
2017-07-03 13:50:45 +0100

    feat(Field) Create Input Field

--- angular/src/app/components/form/fields/input-field/input-field.component.ts
+++ angular/src/app/components/form/fields/input-field/input-field.component.ts
@@ -1,15 +1,125 @@
-import { Component, OnInit } from '@angular/core';
+import {
+	Component,
+	forwardRef,
+	HostBinding,
+	OnInit,
+	ChangeDetectorRef,
+	Input,
+	EventEmitter,
+	Renderer2,
+	ElementRef,
+	Output
+} from '@angular/core';
+import { BaseField } from './../../models/field.model';
+import {
+	NG_VALUE_ACCESSOR,
+	ControlValueAccessor,
+	FormControl
+} from '@angular/forms';
+
+import {
+	CanDisable,
+	mixinDisabled
+} from './../../../../core/common-behaviours/';
+
+const noop = () => {};
+
+/**
+ * Provider Expression that allows df-counter to register as a ControlValueAccessor. This
+ * allows it to support [(ngModel)] and ngControl.
+ */
+
+export const INPUT_FIELD_CONTROL_VALUE_ACCESSOR: any = {
+	provide: NG_VALUE_ACCESSOR,
+	useExisting: forwardRef(() => InputFieldComponent),
+	multi: true
+};
+
+/** A simple change event emitted by the Input Field component. */
+export class InputFieldChange {
+	source: InputFieldComponent;
+	value: string;
+}
+
+// Boilerplate for applying mixins to MdCheckbox.
+/** @docs-private */
+export class InputFieldBase {
+	constructor(public _renderer: Renderer2, public _elementRef: ElementRef) {}
+}
+export const _InputFieldMixinBase = mixinDisabled(InputFieldBase);
 
 @Component({
-  selector: 'input-field',
-  templateUrl: './input-field.component.html',
-  styleUrls: ['./input-field.component.scss']
+	selector: 'input-field',
+	providers: [INPUT_FIELD_CONTROL_VALUE_ACCESSOR],
+	templateUrl: './input-field.component.html',
+	styleUrls: ['./input-field.component.scss']
 })
-export class InputFieldComponent implements OnInit {
+export class InputFieldComponent extends _InputFieldMixinBase
+	implements ControlValueAccessor, OnInit, CanDisable {
+	@Input() question: BaseField<string>;
+	@Input() control: FormControl;
 
-  constructor() { }
+	/** ARIA SETTINGS */
 
-  ngOnInit() {
-  }
+	@Input('aria-invalid')
+	get ariaInvalid(): boolean {
+		return !this.control.valid;
+	}
+	@HostBinding('tabindex') tabIndex = 0;
 
+	/** Internal Value */
+	private _value: string;
+
+	/**
+	 *	Control Value Accessor Default Functions
+	 */
+	onTouched: () => any = () => {};
+
+	private _controlValueAccessorChangeFn: (value: any) => void = () => {};
+
+	constructor(
+		renderer: Renderer2,
+		elementRef: ElementRef,
+		private _changeDetectorRef: ChangeDetectorRef
+	) {
+		super(renderer, elementRef);
+	}
+
+	ngOnInit() {}
+
+	/**
+	* Sets the model value. Implemented as part of ControlValueAccessor.
+	* @param value Value to be set to the model.
+	*/
+	writeValue(value: any) {
+		this._value = value;
+	}
+
+	/**
+	* Registers a callback to be triggered when the value has changed.
+	* Implemented as part of ControlValueAccessor.
+	* @param fn Function to be called on change.
+	*/
+	registerOnChange(fn: (value: any) => void) {
+		this._controlValueAccessorChangeFn = fn;
+	}
+
+	/**
+	* Registers a callback to be triggered when the control has been touched.
+	* Implemented as part of ControlValueAccessor.
+	* @param fn Callback to be triggered when the checkbox is touched.
+	*/
+	registerOnTouched(fn: any) {
+		this.onTouched = fn;
+	}
+
+	/**
+	 * Disables the select. Part of the ControlValueAccessor interface required
+	 * to integrate with Angular's core forms API.
+	 *
+	 * @param isDisabled Sets whether the component is disabled.
+	 */
+	setDisabledState(isDisabled: boolean): void {
+		this.disabled = isDisabled;
+	}
 }
--- angular/src/app/components/form/fields/input-field/input-field.module.ts
+++ angular/src/app/components/form/fields/input-field/input-field.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { InputFieldComponent } from './input-field.component';
 
 @NgModule({
-	imports: [CommonModule],
+	imports: [CommonModule, ReactiveFormsModule],
 	declarations: [InputFieldComponent],
 	exports: [InputFieldComponent]
 })
