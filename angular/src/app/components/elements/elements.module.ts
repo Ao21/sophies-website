774a2709805fd939da6ce87a8ac971a0953b0d03
Ronan Brett <Ronan.Brett@theaa.ie>
2017-07-10 14:32:53 +0100

    chore(Chip) Add Chip module to elements container module

--- angular/src/app/components/elements/elements.module.ts
+++ angular/src/app/components/elements/elements.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { SearchBoxModule } from './search-box/search-box.module';
 import { DropListModule } from './drop-list/drop-list.module';
+import { ChipModule } from './chip/chip.module';
 
 @NgModule({
-	imports: [CommonModule, SearchBoxModule, DropListModule],
+	imports: [CommonModule, SearchBoxModule, DropListModule, ChipModule],
 	declarations: [],
-	exports: [SearchBoxModule, DropListModule]
+	exports: [SearchBoxModule, DropListModule, ChipModule]
 })
 export class ElementsModule {}
