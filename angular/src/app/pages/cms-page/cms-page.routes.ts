b1b4423811cd3ea80cf83949126cc063ff44b96d
Ronan Brett <Ronan.Brett@theaa.ie>
2017-07-14 12:07:08 +0100

    chore(CMS) Add blocks routes

--- angular/src/app/pages/cms-page/cms-page.routes.ts
+++ angular/src/app/pages/cms-page/cms-page.routes.ts
@@ -6,11 +6,33 @@ import { CmsEntryComponent } from './../../components/features/cms-entry/cms-ent
 import { CmsFieldsComponent } from './../../components/features/cms-fields/cms-fields.component';
 import { CmsFieldComponent } from './../../components/features/cms-field/cms-field.component';
 
+import { CmsBlockComponent } from './../../components/features/cms-block/cms-block.component';
+import { CmsBlocksComponent } from './../../components/features/cms-blocks/cms-blocks.component';
+
 import { AssetManagerComponent } from './../../components/objects/asset-manager/asset-manager.component';
 
 import { CheckGetFieldResolve } from './../../core/resolve/checkGetField.resolve';
+import { CheckGetBlockResolve } from './../../core/resolve/checkGetBlock.resolve';
+import { CheckGetEntryResolve} from './../../core/resolve/checkGetEntry.resolve';
 
 export const CmsPageRoutes: Routes = [
+	{
+		path: 'blocks',
+		component: CmsPageComponent,
+		canActivate: [],
+		resolve: {},
+		children: [
+			{
+				path: '',
+				component: CmsBlocksComponent
+			},
+			{
+				path: ':id',
+				resolve: { block: CheckGetBlockResolve },
+				component: CmsBlockComponent
+			}
+		]
+	},
 	{
 		path: 'entries',
 		component: CmsPageComponent,
@@ -23,7 +45,8 @@ export const CmsPageRoutes: Routes = [
 			},
 			{
 				path: ':id',
-				component: CmsEntryComponent
+				component: CmsEntryComponent,
+				resolve: { entry: CheckGetEntryResolve },
 			}
 		]
 	},
